17253fc219c6a7b4c73394b3b245c6214f70e125
thandyn <nguyenphuthan@gmail.com>
2023-03-06 23:59:40 -0800

    added more functions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,79 @@
-// function appendToHistory(search) {
-//   // If there is no search term return the function
-//   if (searchHistory.indexOf(search) !== -1) {
-//     return;
-//   }
-//   searchHistory.push(search);
-// };
+var lon = "";
+var lat = "";
+// forloop
+function renderForecastEl() {
+  // for (var i = 0; i < city.length; i++) {
+  // }
+  var forecastEl = document.createElement("div");
+  forecastEl.className = "card-body m-2 text-center";
+  weatherEl.innerHTML = `
+                <h5>date</h5>
+                <img src="#" alt="weather-icon">
+                <p>temp</p>
+                <p>wind</p>
+                <p>humidity</p>`;
+  document.getElementById("forecast").appendChild(forecastEl);
+}
 
-//   localStorage.setItem('search-history', JSON.stringify(searchHistory));
-//   renderSearchHistory();
+function renderWeatherEl() {
+  // for (var i = 0; i < city.length; i++) {
+  // }
+  var city = "";
+  var img = "";
+  var temp = "";
+  var wind = "";
+  var humidity = "";
+
+  var weatherEl = document.createElement("div");
+  weatherEl.className = "weathertoday";
+  weatherEl.innerHTML = `
+            <div class="card-body text-center">
+              <h2>City</h2>
+              <img src="#" alt="weather-icon">
+              <p>temp</p>
+              <p>wind</p>
+              <p>humidity</p>
+            </div>`;
+  document.getElementById("today").appendChild(weatherEl);
+}
+
+function fetchWeather() {
+  var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=33c71d61d3a49f346c192acaf4618f76`;
+
+  fetch(weatherUrl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var days = data.list;
+      console.log(days);
+      renderWeatherEl();
+      // add dom weather function
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function fetchForecast() {
+  var forecastUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=33c71d61d3a49f346c192acaf4618f76`;
+
+  fetch(forecastUrl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var days = data.list;
+      console.log(days);
+      renderForecastEl();
+      // add dom forecast function
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
 
 document.getElementById("searchBtn").addEventListener("click", function (e) {
   e.preventDefault();
@@ -15,17 +81,23 @@ document.getElementById("searchBtn").addEventListener("click", function (e) {
   if (!cityInput) {
     return;
   }
-  var limit = 5;
-  var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=${limit}&appid=33c71d61d3a49f346c192acaf4618f76`;
+  var limit = 1;
+  var locationUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=${limit}&appid=33c71d61d3a49f346c192acaf4618f76`;
 
-  fetch(requestUrl)
+  fetch(locationUrl)
     .then(function (res) {
       return res.json();
     })
     .then(function (data) {
-      console.log(data);
+      // console.log(data);
+      lon = data[0].lon;
+      lat = data[0].lat;
+      // console.log(lon, lat);
+      fetchForecast();
+      fetchWeather();
     })
     .catch(function (err) {
       console.log(err);
     });
+  // history for loop
 });
